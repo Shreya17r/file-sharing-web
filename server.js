e74fe9c28d4668de30bfcd4697854c580f0c9e8d
Shreya Raghuvansh <52816378+Shreya17r@users.noreply.github.com>
2021-08-03 11:04:50 +0530

    cors update

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 5500;
 
@@ -8,9 +9,18 @@ app.use(express.static('public'));
 
 app.use(express.json());
 
+app.use(cors(corsOptions));
+
+
 const connectDB = require('./config/db');
 connectDB();
 
+//cors 
+
+const corsOptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+
+}
 
 //template engine
 
